8158a56a209d76991be3ba1e1a2bccb892cdbea7
sh-agarwal <shagarwal@yahoo.com>
2020-01-24 23:32:16 +0530

    Added defines (Added IF, THEN etc)

--- CS347-Assignment-1/lex.h
+++ CS347-Assignment-1/lex.h
@@ -7,6 +7,18 @@
 #define NUM_OR_ID	6	/* Decimal Number or Identifier */
 #define MINUS	7	/* - */
 #define DIV	8	/* / */
+#define ASSIGN	9	/* := */
+#define LESS	10	/* < */
+#define GREATER	11	/* > */
+#define EQUALTO	12	/* = */
+#define IF	13	
+#define THEN	14
+#define WHILE	15
+#define DO	16
+#define BEGIN	17
+#define END	18
+#define ID	19
+#define CONSTANT	20
 
 extern char *yytext;		/* in lex.c			*/
 extern int yyleng;
--- CS347-Assignment-1/test.cpp
+++ CS347-Assignment-1/test.cpp
@@ -7,6 +7,7 @@
 int main()
 {
    static char input_buffer[1024];
+   2+3;
    char *current="";
 
    while(!*current ){
@@ -29,65 +30,6 @@ int main()
       return 0;
 }
 
-#include <stdarg.h>
 
-#define MAXFIRST 16
-#define SYNCH   SEMI
-
-int   legal_lookahead(  first_arg )
-int   first_arg;
-{
-    /* Simple error detection and recovery. Arguments are a 0-terminated list of
-     * those tokens that can legitimately come next in the input. If the list is
-     * empty, the end of file must come next. Print an error message if
-     * necessary. Error recovery is performed by discarding all input symbols
-     * until one that's in the input list is found
-     *
-     * Return true if there's no error or if we recovered from the error,
-     * false if we can't recover.
-     */
-
-    va_list    args;
-    int     tok;
-    int     lookaheads[MAXFIRST], *p = lookaheads, *current;
-    int     error_printed = 0;
-    int     rval        = 0;
-
-    va_start( args, first_arg );
-
-    if( !first_arg )
-    {
-   if( match(EOI) )
-       rval = 1;
-    }
-    else
-    {
-   *p++ = first_arg;
-   while( (tok = va_arg(args, int)) && p < &lookaheads[MAXFIRST] )
-       *p++ = tok;
-
-   while( !match( SYNCH ) )
-   {
-       for( current = lookaheads; current < p ; ++current )
-      if( match( *current ) )
-      {
-          rval = 1;
-          goto exit;
-      }
-
-       if( !error_printed )
-       {
-      fprintf( stderr, "Line %d: Syntax error\n", yylineno );
-      error_printed = 1;
-       }
-
-       advance();
-   }
-    }
-
-exit:
-    va_end( args )
-    return rval;
-}
 
 
