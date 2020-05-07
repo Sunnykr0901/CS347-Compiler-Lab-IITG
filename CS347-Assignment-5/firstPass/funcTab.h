895e58b2d279a47e75bbbc19ca240d04d614edf6
sh-agarwal <shagarwal@yahoo.com>
2020-05-07 15:42:13 +0530

    Latest

--- CS347-Assignment-5/firstPass/funcTab.h
+++ CS347-Assignment-5/firstPass/funcTab.h
@@ -9,25 +9,26 @@
 using namespace std;
 
 
-#define FBLU(x) KBLU x RST
-
-#define FYEL(x) KYEL x RST
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
 #define KYEL  "\x1B[33m"
-#define FMAG(x) KMAG x RST
-#define KCYN  "\x1B[36m"
-#define FWHT(x) KWHT x RST
-#define FRED(x) KRED x RST
-#define RST  "\x1B[0m"
-
-#define FCYN(x) KCYN x RST
-#define BOLD(x) "\x1B[1m" x RST
 #define KBLU  "\x1B[34m"
-#define UNDL(x) "\x1B[4m" x RST
-#define KWHT  "\x1B[37m"
-#define FGRN(x) KGRN x RST
-#define KRED  "\x1B[31m"
 #define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLD(x) "\x1B[1m" x RST
+#define UNDL(x) "\x1B[4m" x RST
+
 
 enum varType {SIMPLE, ARRAY};
 
