7f778a29a999dbd81e52ca1bfade44c100f190a8
sh-agarwal <shagarwal@yahoo.com>
2020-05-07 13:50:37 +0530

    Some Changes

--- CS347-Assignment-5/firstPass/codegenHelpers.c
+++ CS347-Assignment-5/firstPass/codegenHelpers.c
@@ -1,65 +1,12 @@
 #include "codegenHelpers.h"
 
-string registerSet::getRegister() {
-    string reg = "";
-    if (tempRegister.size()==0) {
-        cout << BOLD(FRED("FATAL ERROR : Exceeded maximum temporary Int registers")) << endl;
-        exit(1);
-        return reg;
-    }
-    reg += "T";
-    int x = tempRegister[tempRegister.size()-1];
-    reg += to_string(x);
-    tempRegister.pop_back();
-    return reg;
-}
-
-string registerSet::getFloatRegister() {
-    string reg = "";
-    if (floatRegister.size()==0) {
-        cout << BOLD(FRED("FATAL ERROR : Exceeded maximum temporary Float registers")) << endl;
-        exit(1);
-        return reg;
-    }
-    reg += "F";
-    int x = floatRegister[floatRegister.size()-1];
-    reg += to_string(x);
-    floatRegister.pop_back();
-    return reg;
-}
+/*Added by SSR*/
 
 
-void registerSet::freeRegister(string s){
-    if(s[0]=='F'){
-        s[0] = '0';
-        int x = stoi(s);
-        for(auto it : floatRegister){
-            if(it==x){
-                // cout<<"Trying to free an already freed Float Register "<<s<<endl;
-                return;
-            }
-        }
-        // cout<<"FLoat Register Freed "<< s <<endl;
-        floatRegister.push_back(x);
-    } else if(s[0] == 'T'){
-        s[0] = '0';
-        int x = stoi(s);
-        for(auto it:tempRegister){
-            if(it==x){
-                // cout<<"Trying to free an already freed Int Register "<<s<<endl;
-                return;
-            }
-        }
-        // cout<<"Int Register Freed "<< s <<endl;
-        tempRegister.push_back(x);
-    } else {
-        cout << "Not a Temp Variable : " << s << endl;
-    }
-}
 
 void merge(vector<int> *&receiver, vector<int> *&donor) {
     if(donor==NULL || receiver == NULL){
-        // cout<<"Conitnued because vector empty"<<endl;
+        
         return;
     }
     for(int i:(*donor)){
@@ -71,7 +18,7 @@ void merge(vector<int> *&receiver, vector<int> *&donor) {
 
 void mergeSwitch(vector<pair<string,int>> *&receiver,vector<pair<string,int>> *&donor) {
     if(donor==NULL || receiver == NULL){
-        // cout<<"Conitnued because vector empty"<<endl;
+        
         return;
     }
     for(auto i:(*donor)){
@@ -81,15 +28,76 @@ void mergeSwitch(vector<pair<string,int>> *&receiver,vector<pair<string,int>> *&
     return;
 }
 
+/*Added by Sid and Shubham */
+
+string registerSet::getRegister() {
+    string reg = "";
+    if (tempRegister.size()==0) {
+        cout << BOLD( FRED ("FATAL ERROR : Exceeded maximum temporary Int registers") ) << endl;
+        exit(1);
+        return reg;
+    }
+    reg += "T";
+    int x = tempRegister[tempRegister.size()-1];
+    reg += to_string(x);
+    tempRegister.pop_back();
+    return reg;
+}
+
+
+string registerSet::getFloatRegister() {
+    string reg = "";
+    if (floatRegister.size()==0) {
+        cout << BOLD (FRED ("FATAL ERROR : Exceeded maximum temporary Float registers") ) << endl;
+        exit(1);
+        return reg;
+    }
+    reg += "F";
+    int x = floatRegister[floatRegister.size()-1];
+    reg += to_string(x);
+    floatRegister.pop_back();
+    return reg;
+}
+
+
+
+void registerSet::freeRegister(string s){
+    if(s[0]=='F'){
+        s[0] = '0';
+        int x = stoi(s);
+        for(auto it : floatRegister){
+            if(it==x){
+                
+                return;
+            }
+        }
+        
+        floatRegister.push_back(x);
+    } else if(s[0] == 'T'){
+        s[0] = '0';
+        int x = stoi(s);
+        for(auto it:tempRegister){
+            if(it==x){
+                
+                return;
+            }
+        }
+        
+        tempRegister.push_back(x);
+    } else {
+        cout << "Not a Temp Variable : " << s << endl;
+    }
+}
+/*Added by SSR*/
+
 void backpatch(vector<int> *&lineNumbers, int labelNumber, vector<string> &functionInstruction){
     if(lineNumbers == NULL){
-        cout << "Given line numbers for "<<labelNumber<<" is NULL"<<endl;
+        cout << "Written Line Number for: "<<labelNumber<<" is NULL"<<endl;
         return;
     }
     string statement;
     for(int it : (*lineNumbers)){
-        // statement = functionInstruction[it];        // statement +=("L"+ to_string(labelNumber));
-        functionInstruction[it] += (to_string(labelNumber));
+        
     }
     lineNumbers->clear();
 }
@@ -99,7 +107,7 @@ void backpatch(vector<int> *&lineNumbers, int labelNumber, vector<string> &funct
 void gen(vector<string> &functionInstruction, string instruction, int &nextQuad){
     functionInstruction.push_back(instruction);
     nextQuad++;
-    // cout << instruction << endl;
+    
     return;
 }
 
