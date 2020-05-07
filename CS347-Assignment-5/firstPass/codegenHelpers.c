5462a34bc37df3a2fb5524ed57833c16b2f259e4
sh-agarwal <shagarwal@yahoo.com>
2020-05-07 15:51:35 +0530

    Update: Reverted Codegenhelper.c

--- CS347-Assignment-5/firstPass/codegenHelpers.c
+++ CS347-Assignment-5/firstPass/codegenHelpers.c
@@ -1,39 +1,9 @@
 #include "codegenHelpers.h"
 
-/*Added by SSR*/
-
-
-
-void merge(vector<int> *&receiver, vector<int> *&donor) {
-    if(donor==NULL || receiver == NULL){
-        
-        return;
-    }
-    for(int i:(*donor)){
-        receiver->push_back(i);
-    }
-    donor->clear();
-    return;
-}
-
-void mergeSwitch(vector<pair<string,int>> *&receiver,vector<pair<string,int>> *&donor) {
-    if(donor==NULL || receiver == NULL){
-        
-        return;
-    }
-    for(auto i:(*donor)){
-        receiver->push_back(i);
-    }
-    donor->clear();
-    return;
-}
-
-/*Added by Sid and Shubham */
-
 string registerSet::getRegister() {
     string reg = "";
     if (tempRegister.size()==0) {
-        cout << BOLD( FRED ("FATAL ERROR : Exceeded maximum temporary Int registers") ) << endl;
+        cout << BOLD(FRED("FATAL ERROR : Exceeded maximum temporary Int registers")) << endl;
         exit(1);
         return reg;
     }
@@ -44,11 +14,10 @@ string registerSet::getRegister() {
     return reg;
 }
 
-
 string registerSet::getFloatRegister() {
     string reg = "";
     if (floatRegister.size()==0) {
-        cout << BOLD (FRED ("FATAL ERROR : Exceeded maximum temporary Float registers") ) << endl;
+        cout << BOLD(FRED("FATAL ERROR : Exceeded maximum temporary Float registers")) << endl;
         exit(1);
         return reg;
     }
@@ -60,54 +29,74 @@ string registerSet::getFloatRegister() {
 }
 
 
-
 void registerSet::freeRegister(string s){
     if(s[0]=='F'){
         s[0] = '0';
         int x = stoi(s);
         for(auto it : floatRegister){
             if(it==x){
-                
+                // cout<<"Trying to free an already freed Float Register "<<s<<endl;
                 return;
             }
         }
-        
+        // cout<<"FLoat Register Freed "<< s <<endl;
         floatRegister.push_back(x);
     } else if(s[0] == 'T'){
         s[0] = '0';
         int x = stoi(s);
         for(auto it:tempRegister){
             if(it==x){
-                
+                // cout<<"Trying to free an already freed Int Register "<<s<<endl;
                 return;
             }
         }
-        
+        // cout<<"Int Register Freed "<< s <<endl;
         tempRegister.push_back(x);
     } else {
         cout << "Not a Temp Variable : " << s << endl;
     }
 }
-/*Added by SSR*/
-
-void backpatch(vector<int> *&lineNumbers, int labelNumber, vector<string> &functionInstruction){
-    if(lineNumbers == NULL){
-        cout << "Written Line Number for: "<<labelNumber<<" is NULL"<<endl;
-        return;
-    }
-    string statement;
-    for(int it : (*lineNumbers)){
-        
-    }
-    lineNumbers->clear();
-}
-
-
 
 void gen(vector<string> &functionInstruction, string instruction, int &nextQuad){
     functionInstruction.push_back(instruction);
     nextQuad++;
-    
+    // cout << instruction << endl;
     return;
 }
 
+void backpatch(vector<int> *&lineNumbers, int labelNumber, vector<string> &functionInstruction){
+    if(lineNumbers == NULL){
+        cout << "Given line numbers for "<<labelNumber<<" is NULL"<<endl;
+        return;
+    }
+    string statement;
+    for(int it : (*lineNumbers)){
+        // statement = functionInstruction[it];        // statement +=("L"+ to_string(labelNumber));
+        functionInstruction[it] += (to_string(labelNumber));
+    }
+    lineNumbers->clear();
+}
+
+void merge(vector<int> *&receiver, vector<int> *&donor) {
+    if(donor==NULL || receiver == NULL){
+        // cout<<"Conitnued because vector empty"<<endl;
+        return;
+    }
+    for(int i:(*donor)){
+        receiver->push_back(i);
+    }
+    donor->clear();
+    return;
+}
+
+void mergeSwitch(vector<pair<string,int>> *&receiver,vector<pair<string,int>> *&donor) {
+    if(donor==NULL || receiver == NULL){
+        // cout<<"Conitnued because vector empty"<<endl;
+        return;
+    }
+    for(auto i:(*donor)){
+        receiver->push_back(i);
+    }
+    donor->clear();
+    return;
+}
