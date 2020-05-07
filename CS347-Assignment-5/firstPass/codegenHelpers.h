1ead4adb9f78682e1d054f83779e5c1775a14177
sh-agarwal <shagarwal@yahoo.com>
2020-05-07 13:31:36 +0530

    Changed codegenHelpers.h

--- CS347-Assignment-5/firstPass/codegenHelpers.h
+++ CS347-Assignment-5/firstPass/codegenHelpers.h
@@ -1,32 +1,31 @@
 #pragma once
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <stdio.h>
-// #include <algorithm>
 #include <utility>
+#include <vector>
 #include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <stack>
 using namespace std;
 
-#define RST  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
 #define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-
 #define FRED(x) KRED x RST
 #define FGRN(x) KGRN x RST
-#define FYEL(x) KYEL x RST
-#define FBLU(x) KBLU x RST
-#define FMAG(x) KMAG x RST
-#define FCYN(x) KCYN x RST
-#define FWHT(x) KWHT x RST
 
+#define FMAG(x) KMAG x RST
+#define KWHT  "\x1B[37m"
+
+#define RST  "\x1B[0m"
+#define FBLU(x) KBLU x RST
+#define FYEL(x) KYEL x RST
+#define KYEL  "\x1B[33m"
 #define BOLD(x) "\x1B[1m" x RST
 #define UNDL(x) "\x1B[4m" x RST
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define FWHT(x) KWHT x RST
+#define FCYN(x) KCYN x RST
 
 class registerSet {
 private:
@@ -51,8 +50,9 @@ public:
     void freeRegister(string s);
 };
 
-
-void gen(vector<string> &, string ,int &);
-void backpatch(vector<int> *&, int, vector<string> &);
-void merge(vector<int> *&, vector<int> *&);
 void mergeSwitch(vector<pair<string,int>> *&receiver,vector<pair<string,int>> *&donor); 
+void backpatch(vector<int> *&, int, vector<string> &);
+void gen(vector<string> &, string ,int &);
+void merge(vector<int> *&, vector<int> *&);
+
+ 
